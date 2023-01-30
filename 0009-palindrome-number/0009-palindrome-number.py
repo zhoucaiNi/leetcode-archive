3520253a6499f9263591b09bd97edb2cf0312408
Zhoucai <nizhoucai@gmail.com>
2023-01-30 00:33:27 -0500

    Time: 50 ms (87.15%), Space: 13.3 MB (90.11%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if str(x)[::-1] == str(x):
+            return True
+        return False
