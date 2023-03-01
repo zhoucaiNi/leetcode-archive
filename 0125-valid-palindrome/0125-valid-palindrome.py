e8d0a2eaee9a504974217ed0f175f658571f9287
Zhoucai <nizhoucai@gmail.com>
2023-02-28 22:57:33 -0500

    Time: 574 ms (7.53%), Space: 16.1 MB (16.78%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        real_s = ""
+        for char in s:
+            if char.isalnum():
+                stack.append(char.lower())
+                real_s += char.lower()
+            
+        t = ""
+        while stack:
+            t = t+ stack.pop()
+        print(t)
+        print(real_s)
+        return t == real_s
