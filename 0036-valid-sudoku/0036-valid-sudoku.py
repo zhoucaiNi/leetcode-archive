52df457a24949bef4f9bc95c3328531c674b5fd0
Zhoucai <nizhoucai@gmail.com>
2023-06-10 13:45:55 -0400

    Time: 129 ms (10.04%), Space: 16.5 MB (9.42%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,24 @@
+# from collections import defaultdict
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        s = [set() for _ in range(len(board) *3)]
+
+        for x in range(len(board)):
+            for y in range(len(board[0])):
+                currVal = board[x][y]
+                if currVal in s[x] or currVal in s[9+y] or \
+                currVal in s[18+int(x/3) + 3* int(y/3)]:
+                    print(int(x/3) + 3* int(y/3))
+                    print(currVal)
+                    print(x,y)
+                    print(s)
+                    return False
+                elif board[x][y] != ".":
+                    s[x].add(currVal)
+                    s[y+9].add(currVal)
+                    s[18+int(x/3) + 3* int(y/3)].add(currVal)
+        print(s)
+        return True
+                    
+                
+        
