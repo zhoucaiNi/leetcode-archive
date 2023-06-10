6eda52e2b0b563230d7919e1a267afd837a770d6
Zhoucai <nizhoucai@gmail.com>
2023-06-10 15:13:33 -0400

    Time: 24 ms (23.14%), Space: 13.4 MB (93.61%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -10,7 +10,7 @@ class Solution(object):
         for char in s:
             if char in d.values():
                 stack.append(char)
-            elif char in d:
+            elif char in d.keys():
                 if stack == [] or d[char] != stack.pop():
                     return False
             else:
