ee2a7e6c992789e700eaed85d77307ec6fd35f66
Zhoucai <nizhoucai@gmail.com>
2023-06-10 14:12:18 -0400

    Time: 50 ms (82.86%), Space: 13.9 MB (95.07%) - LeetHub

---
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -0,0 +1,28 @@
+class Codec:
+
+    def encode(self, strs):
+        """Encodes a list of strings to a single string.
+        
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0:
+            return unichr(258)
+        
+        return unichr(257).join(x.encode("utf-8") for x in strs)
+        
+
+    def decode(self, s):
+        """Decodes a single string to a list of strings.
+        
+        :type s: str
+        :rtype: List[str]
+        """
+        if s == unichr(258):
+            return []
+        return s.split(unichr(257))
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
