a76ec41df05e7546512a4974bd084238ea1adf4b
Zhoucai <nizhoucai@gmail.com>
2023-06-17 15:50:42 -0400

    Time: 85 ms (69.33%), Space: 13.8 MB (31.59%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,21 +5,26 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        count = {}
+        l, count= 0, {}
         res = 0
-
-        l = 0
         maxf = 0
         for r in range(len(s)):
+            # check for condition
             count[s[r]] = 1 + count.get(s[r], 0)
             maxf = max(maxf, count[s[r]])
-
-            if (r - l + 1) - maxf > k:
+            
+            
+            if (r - l + 1 - maxf) > k:
                 count[s[l]] -= 1
-                l += 1
-
+                if not count[s[l]]:
+                    count.pop(s[l])
+                l+=1
+                
             res = max(res, r - l + 1)
-        return res
+
+        return res 
+    
+      
                 
     
         
