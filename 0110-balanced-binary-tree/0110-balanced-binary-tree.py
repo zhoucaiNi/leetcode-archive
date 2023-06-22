689eeca00e9e9bc4e45cf5043e4fbda92d188b51
Zhoucai <nizhoucai@gmail.com>
2023-06-22 11:46:50 -0400

    Time: 43 ms (33.56%), Space: 18.4 MB (36.96%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        
+        def dfs(root):
+            if not root:
+                return 0
+
+            left = dfs(root.left)
+            right = dfs(root.right)
+ 
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+            
+            return max(left, right) + 1
+        
+        return dfs(root) != -1
+        
