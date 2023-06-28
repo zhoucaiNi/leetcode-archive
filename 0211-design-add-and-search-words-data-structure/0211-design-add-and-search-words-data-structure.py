e38d0da20adab729e548daefbc40be0d7a168520
Zhoucai <nizhoucai@gmail.com>
2023-06-28 11:33:17 -0400

    Time: 1595 ms (94.55%), Space: 58.4 MB (86.62%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,50 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.root = {}
+        
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            try:
+                cur = cur[c]
+            except:
+                cur[c] = {}
+                cur = cur[c]
+        cur["word"] = {}
+            
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        
+        def dfs(index, root):
+            cur = root
+            if index < len(word):
+                if word[index] == ".":
+                    for key in cur:
+                        if dfs(index + 1, cur[key]):
+                            return True
+                    return False
+                
+                if word[index] not in cur:
+                    return False
+                    
+            if index == len(word):
+                try:
+                    return "word" in cur
+                except:
+                    return False
+            cur = cur[word[index]]
+            return dfs(index + 1, cur)
+    
+        return dfs(0, cur)
+                
+            
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
