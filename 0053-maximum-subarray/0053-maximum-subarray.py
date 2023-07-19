efa1ad94468ac9bb3ccb999591ee8b5675e643b7
Zhoucai <nizhoucai@gmail.com>
2023-07-19 12:43:31 -0400

    Time: 695 ms (74.66%), Space: 30.4 MB (91.68%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,13 +1,13 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        # Kadane's algo
-        t = k = nums[0]
-        for i in nums[1:]:
-            t = max(i, t+i)
-            k = max(k, t)
-        return k
-
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSub = nums[0]
+        currSum = 0
+        
+        for n in nums:
+            if currSum < 0:
+                currSum = 0
+            currSum += n
+            maxSub = max(maxSub, currSum)
+        
+        return maxSub
+        
