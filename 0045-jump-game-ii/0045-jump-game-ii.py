5ceb579842ee8d40c466099d6fba53012fcc5eb1
Zhoucai <nizhoucai@gmail.com>
2023-07-20 14:15:28 -0400

    Time: 11449 ms (5.01%), Space: 17.6 MB (20.17%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -3,16 +3,11 @@ class Solution:
         row = [None] * len(nums)
         row[-1] = 0
         for i in range(len(nums)-2, -1, -1):
-            # print(nums[i])
             for j in range(1, nums[i]+1):
-                # print(i+j)
-                # print(len(row))
                 if i+j < len(row) and row[i+j] != None:
                     if row[i] == None:
                         row[i] = 1 + row[i+j]
                     else:
                         row[i] = min(row[i], row[i+j]+1)
-        print(row)
-        
         return row[0]
         
