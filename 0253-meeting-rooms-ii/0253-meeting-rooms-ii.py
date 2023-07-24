c8e244abeac2848378d36467f1e400b70f7afdf2
Zhoucai <nizhoucai@gmail.com>
2023-07-24 13:58:57 -0400

    Time: 114 ms (5.68%), Space: 20.2 MB (25.46%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        start = []
+        end = []
+        
+        for iStart, iEnd in intervals:
+            start.append(iStart)
+            end.append(iEnd)
+            
+        start.sort()
+        end.sort()
+        
+        
+        count, res = 0,0 
+        s, e = 0,0 
+        for i in range(len(start)* 2 - 1):
+            print(i)
+            print(s, e)
+            if  s == len(start) or end[e] <= start[s]:
+                e+=1
+                count-=1
+            else:
+                s+=1
+                count+=1
+                res = max(count, res)
+                
+            
+        return res
+        print(start)
+        print(end)
