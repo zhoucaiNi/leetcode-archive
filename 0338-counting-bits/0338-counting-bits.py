8657508692c72151f7e36cc65c85b034d1641cb5
Zhoucai <nizhoucai@gmail.com>
2023-07-26 12:49:24 -0400

    Time: 84 ms (94.52%), Space: 23 MB (71.56%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,13 +1,12 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        def hamminglength(t):
-            res = 0
-            while t:
-                res += t % 2
-                t = t >> 1
-            return res
-        res = []
-        for i in range(n+1):
-            res.append(hamminglength(i))
+        dp = [0] * (n + 1)
+        offset = 1
+        
+        for i in range(1, n+1):
+            if offset * 2 == i:
+                offset = i
             
-        return res
+            dp[i] = 1 + dp[i - offset]
+            
+        return dp
