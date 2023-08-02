a7e5698925c1dbc36cee74fb2c7aa551736c2ca5
Zhoucai <nizhoucai@gmail.com>
2023-08-02 16:34:00 -0400

    Time: 36 ms (92.09%), Space: 16.3 MB (35.22%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,14 +1,17 @@
-class Solution(object):
-    def getSum(self, a, b):
-        """
-        :type a: int
-        :type b: int
-        :rtype: int
-        """
-        mask = 0xFFFFFFFF
-        while (b & mask) > 0:
-            c = (a&b) << 1
-            a = (a^b)
-            b = c
-      
-        return (a & mask) if b > 0 else a 
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        def add(a, b):
+            if not a or not b:
+                return a or b
+            
+            return add(a ^ b, (a & b) << 1)
+        
+        if a * b < 0:  # assume a < 0, b > 0
+            if a > 0:
+                return self.getSum(b, a)
+            if add(~a, 1) == b:  # -a == b
+                return 0
+            if add(~a, 1) < b:  # -a < b
+                return add(~add(add(~a, 1), add(~b, 1)), 1)  # -add(-a, -b)
+        
+        return add(a,b)
