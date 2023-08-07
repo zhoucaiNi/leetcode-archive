ce786839c55b1e82e6612bc072ef61270f47d29c
Zhoucai <nizhoucai@gmail.com>
2023-08-07 15:11:39 -0400

    Time: 211 ms (41.12%), Space: 17.5 MB (95.24%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,50 +1,18 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        adjList= {i: [] for i in range(n)} 
+        prices = [float("inf")] * n
+        prices[src] = 0
         
-        for start, end, price in flights:
-            adjList[start].append([end, price])
-
-        print(adjList)
-        minHeap = []
-        for i in range(len(adjList[src])):
-            minHeap.append([adjList[src][i][1], adjList[src][i][0]])
-                
-        print(minHeap)
-        res = float("inf")
-        seen = set()
-        while k >= 0:
-
-            t = len(minHeap)
-            newList = []
-            for i in range(t):
-                # print(minHeap)
-                totalCost, curr = heapq.heappop(minHeap)
-                # print(totalCost, curr)
-                if curr == dst:     
-                    res = min(res, totalCost)
-
-                if (totalCost, curr) in seen:
+        print(prices)
+        
+        for i in range(k+1):
+            tmpPrices = prices.copy()
+            for s, d, p in flights:
+                if prices[s] == float("inf"):
                     continue
-                
-                seen.add((totalCost, curr))
-                for i in range(len(adjList[curr])):
-                    if totalCost + adjList[curr][i][1] < res:
-                        newList.append([totalCost + adjList[curr][i][1], adjList[curr][i][0]])
-                    
-            for l in newList:
-                heapq.heappush(minHeap, l)
-                
-            k-=1
-                
-        return res if res != float("inf") else -1
+                if prices[s] + p < tmpPrices[d]:
+                    tmpPrices[d] = prices[s] + p
             
-            
-            
-            
-            
-            
-            
-        
-        
+            prices = tmpPrices
+        return prices[dst] if prices[dst] != float("inf") else -1
         
