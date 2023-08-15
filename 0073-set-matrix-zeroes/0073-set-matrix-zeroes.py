80c58703cfabc5ab3b661c56ba1aea95562df0ba
Zhoucai <nizhoucai@gmail.com>
2023-08-14 22:12:09 -0400

    Time: 113 ms (93.18%), Space: 17.2 MB (56.92%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,24 +4,26 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         row, col = len(matrix), len(matrix[0])
-        
-        directions = [(0,1), (0,-1), (1,0), (-1,0)]
-        
-        seen = set()
+        rowZero = False
         
         for i in range(row):
             for j in range(col):
-                if matrix[i][j] == 0 and (i, j) not in seen :
-                    k = 0
-                    newI, newJ = i, j
-                    while k < len(directions):
-                        r,c = directions[k][0], directions[k][1]
-                        newI, newJ = newI + r, newJ + c
-                        if (newI not in range(row) or 
-                            newJ not in range(col)):
-                            newI, newJ = i,j
-                            k+=1
-                        else:
-                            if matrix[newI][newJ] != 0:
-                                seen.add((newI, newJ))
-                                matrix[newI][newJ] = 0
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    if i > 0:
+                        matrix[i][0] = 0
+                    else:
+                        rowZero = True
+                        
+        for r in range(1, row):
+            for c in range(1, col):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+                    
+        if matrix[0][0] == 0:
+            for r in range(row):
+                matrix[r][0] = 0
+                    
+        if rowZero:
+            for c in range(col):
+                matrix[0][c] = 0
