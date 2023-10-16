840f12a57acf1dbf6df5c903c153aa6f0c54fcbe
Zhoucai <nizhoucai@gmail.com>
2023-10-16 00:10:03 -0400

    Time: 41 ms (33.2%), Space: 16.1 MB (95.85%) - LeetHub

--- 1965-sum-of-digits-in-base-k/1965-sum-of-digits-in-base-k.py
+++ 1965-sum-of-digits-in-base-k/1965-sum-of-digits-in-base-k.py
@@ -2,7 +2,7 @@ class Solution:
     def sumBase(self, n: int, k: int) -> int:
         rSum = 0
         while (n>0):
-            print((n % k))
+            # print((n % k))
             rSum += (n % k)
             n = n // k 
 
