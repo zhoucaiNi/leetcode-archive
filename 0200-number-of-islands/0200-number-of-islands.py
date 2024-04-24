ae942031d222a2965c45038d098af0d1e19b1512
Zhoucai <nizhoucai@gmail.com>
2024-04-24 03:43:36 -0400

    Time: 360 ms (5.16%), Space: 24.2 MB (27.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,43 +1,34 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
+        
+        if not grid or not grid[0]:
             return 0
         
-        ROWS, COLS = len(grid), len(grid[0])
         visited = set()
         island = 0
-        
-        def bfs(r, c):
-            q = collections.deque()
+        rows, cols = len(grid), len(grid[0])
+    
+        def dfs(r,c):
+            if (
+                r not in range(rows) or
+                c not in range(cols) or
+                (r,c) in visited or
+                grid[r][c] == "0"
+            ):
+                return 
+
             visited.add((r,c))
-            q.append((r,c))
-            
-            direction = [[0,1], [0,-1], [1,0], [-1,0]]
-            while q:
-               
-                row,col = q.popleft()
-                for dr, dc in direction:
-                    r, c = row + dr, col + dc
-                    # print((r,c))
-                    if (r in range(ROWS) and 
-                        c in range(COLS) and 
-                        grid[r][c] == "1" and
-                        (r,c) not in visited):
-                            q.append((r,c))
-                            visited.add((r,c))
-        
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (r,c) not in visited and grid[r][c] == "1":
-                    bfs(r,c)
-                    # print(visited)
-                    island += 1
+            directions = [[1,0], [-1,0], [0,1], [0,-1]]
+            for dr, dc in directions:
+                dfs(r+dr, c+dc)
+                
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visited: 
+                    island +=1
+                    dfs(r,c)
                     
         return island
-    
-        
-                    
-                    
-                
-        
+            
+
         
